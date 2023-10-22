8a2f31bdb0e664fda400c79938edbf718eec7850
Anderson-Brant <31324042+Anderson-Brant@users.noreply.github.com>
2023-10-21 17:00:36 -0700

    Refactor data collection functions
    - Modified data collection functions in data_collection.py
    - Added support for user-defined stock symbols
    - Improved comments and documentation
    - Included a condition for controlling the update loop in update_real_time_data
    - Enhanced code flexibility and user-friendliness

--- data_collection.py
+++ data_collection.py
@@ -2,12 +2,12 @@
 Data Collection Module
 Author: [Brant Anderson]
 Description: This module is responsible for collecting historical and real-time stock price data from Yahoo Finance.
-Date: [10/7/23]
+Date: [10/21/23]
 first edition
 """
 
-import pandas as pd
 import yfinance as yf
+import pandas as pd
 import time
 
 def download_historical_data(stock_symbol, start_date, end_date):
@@ -25,7 +25,7 @@ def download_historical_data(stock_symbol, start_date, end_date):
     data = yf.download(stock_symbol, start=start_date, end=end_date)
     return data
 
-def update_real_time_data(stock_symbol, data, max_length, update_interval=60):
+def update_real_time_data(stock_symbol, data, max_length, update_interval=60, max_updates=5):
     """
     Update the existing dataset with real-time stock price data for a given stock symbol.
 
@@ -34,10 +34,13 @@ def update_real_time_data(stock_symbol, data, max_length, update_interval=60):
         data (pd.DataFrame): The existing DataFrame containing historical data.
         max_length (int): The maximum length of the dataset to manage memory usage.
         update_interval (int): The time interval in seconds for updating real-time data.
+        max_updates (int): Maximum number of updates (optional).
 
     Returns:
         pd.DataFrame: The updated DataFrame with real-time data appended.
     """
+    updates = 0
+
     while True:
         real_time_data = yf.download(stock_symbol, period="1d", interval="1m")
         data = data.append(real_time_data)
@@ -46,8 +49,12 @@ def update_real_time_data(stock_symbol, data, max_length, update_interval=60):
         if len(data) > max_length:
             data = data.iloc[-max_length:]
         
+        updates += 1
+
+        if max_updates is not None and updates >= max_updates:
+            break
+
         time.sleep(update_interval)
 
-        # You can add a condition to exit the loop or limit the number of updates
-
     return data
+
